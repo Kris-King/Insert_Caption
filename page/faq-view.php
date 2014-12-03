7d2e407ce4165050d59da9ccd76549395184c27b
kristopher.king <krisking120@gmail.com>
2014-12-03 15:58:40 +1300

    Merge origin/master
    Conflicts:
    	nbproject/private/private.xml

--- page/faq-view.php
+++ page/faq-view.php
@@ -1,7 +1,9 @@
 
 <h1>FAQ - Frequently Asked Questions</h1>
 
-<p></p>
+<p>Below you can find a list of commonly asked questions however if you can not find
+what you looking for, you can go to the Help page for more indepth
+explanations. If you are still lost, do not hesitate in contacting us.</p>
 
 <p>Q.What can I submit</p>
 <p>A.Amusing Images which others can caption</p>
