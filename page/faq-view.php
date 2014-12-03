d89e65b26525f2c39f46511c2fe05e1bde95ad42
kristopher.king <krisking120@gmail.com>
2014-12-03 19:09:15 +1300

    Merge origin/master
    Conflicts:
    	nbproject/private/private.xml

--- page/faq-view.php
+++ page/faq-view.php
@@ -2,8 +2,111 @@
 <h1>FAQ - Frequently Asked Questions</h1>
 
 <p>Below you can find a list of commonly asked questions however if you can not find
-what you looking for, you can go to the Help page for more indepth
-explanations. If you are still lost, do not hesitate in contacting us.</p>
+    what you looking for, you can go to the Help page for more indepth
+    explanations. If you are still lost, do not hesitate in contacting us.</p>
 
-<p>Q.What can I submit</p>
-<p>A.Amusing Images which others can caption</p>
+<h3>1.</h3>
+
+<p>Q. What can I submit?</p>
+<p>A. Amusing Images which others can caption</p>
+
+<h3>2.</h3>
+
+
+<p>Q. What types of images can I submit?</p>
+<p>A. .jpg, .gif and .png</p>
+
+<h3>3.</h3>
+
+
+<p>Q. Can anyone post images and captions?</p>
+<p>A. No, you must be a registered user to caption and upload images</p>
+
+<h3>4.</h3>
+
+
+<p>Q. Do I need to pay to create an account?</p>
+<p>A. No, users can create a free account without the worry of having to pay</p>
+
+<h3>5.</h3>
+
+
+<p>Q. Does uploading images or captions fall under a paygate?</p>
+<p>A. No, registered users can upload and caption without monetary barriers.</p>
+
+
+
+<h3>6.</h3>
+
+
+<p>Q. Can I link to an image from another site for others to caption?</p>
+<p>A. No, you must save the image to your computer and then upload it.</p>
+
+
+
+<h3>7.</h3>
+
+
+<p>Q. Are registered users able to decide how popular an image is?</p>
+<p>A. Yes, images have a rating system attached to it giving 
+    registered users the ability to influence how popular
+    an image is. All users then have the ability to view 
+    images on the front page by most recent or most popular.</p>
+
+
+
+<h3>8.</h3>
+
+
+<p>Q. How do I report an offensive image or caption?</p>
+<p>A. Go to our <a href="http://localhost/InsertCaption/web/index.php?page=contact">contact page</a> and inform us of the image. Please
+    provide a link to the image so we can investigate the matter further.</p>
+
+
+
+<h3>9.</h3>
+
+
+<p>Q. What is your policy on copyrighted material?</p>
+<p>A. We will not be held responsible for the images uploaded by users on this site.
+    Any image which is issued a copyright notice will be pulled immediately. You
+    can find more information on this by going to the <a href="http://localhost/InsertCaption/web/index.php?page=help">
+        help page</a> and looking under Copyright</p>
+
+
+
+<h3>10.</h3>
+
+
+<p>Q. What if I've been harassed by another user?</p>
+<p>A. If another user is harassing you in anyway i.e posting threatening
+    or sexually explicit comments please <a href="http://localhost/InsertCaption/web/index.php?page=contact">contact us</a> 
+    immediately and inform us of the user that is posting these comments</p>
+
+
+
+<h3>11.</h3>
+
+
+<p>Q. How do I contest a complaint made by another user?</p>
+<p>A. If you believe you been unfairly targeted by another user and wish to contest
+    it, please <a href="http://localhost/InsertCaption/web/index.php?page=contact">contact us</a> 
+    and provide a justified explanation to why no action should be taken. Any user who has been 
+    reported will be contacted before any ruling is undertaken and given an opportunity to explain 
+    themselves. If no justified response is given then measures will be taken to remedy the 
+    situation</p>
+
+
+
+<h3>12.</h3>
+
+
+<p>Q. What if I post an image which is deemed objectionable?</p>
+<p>A. Anyone who posts images which are objectionable or offensive in any way will have their images
+    pulled from the site. If said image is overly offensive in any matter, then the user
+    will be banned and reported to the authorities. This site will not tolerate images of this nature
+    and will pursue those responsible.</p>
+
+
+<p>For more assistance, please visit the <a href="http://localhost/InsertCaption/web/index.php?page=help">
+        Help page</a> for more information.</p>
--- page/help-view.php
+++ page/help-view.php
@@ -1,3 +1,56 @@
-<p>Below is a list of helpful hints to assist you when using
-our site.</p>
+<h1>Help</h1>
 
+
+
+<p>Below is a list of helpful hints to assist you when using
+    our site.</p>
+
+<h3>Uploading Images</h3>
+
+
+<p> - Only registered users can upload images. Those that wish to upload
+    their own images must first create an account.</p>
+
+<p> - Users can upload images which are one of the following: jpg, gif and png.</p>
+
+<p> - Please be wary of any images you upload, other users may find certain images offensive.</p>
+
+<p> - If you have any issues uploading images to the site please 
+    <a href="http://localhost/InsertCaption/web/index.php?page=contact">contact us</a>.
+    When you do submit an error report please mention what you were doing that led up to this 
+    moment and the error message you received.</p>
+
+<p> - If an image you uploaded is deemed offensive in any matter you will be sent
+    an email explaining why and given instructions should you wish to contest this.</p>
+
+
+<h3>Captions</h3>
+
+<p> - Registered users can create captions for any image they see on this site.</p>
+
+<p> - We will pull any caption which is anyway threatening or sexually explicit.</p>
+
+<p> - We will not tolerate those who use captions to bully other users.</p>
+
+<p> - We will report any user who tries to blackmail others through submitted captions.</p>
+
+<p> - There is no limit on how many captions a user can make on a single image.</p>
+
+
+<h3>Image Rating</h3>
+
+<p> - Users with an account have the option of Upvoting or Downvoting images they like or dislike.</p>
+
+<p> - Users both registered or otherwise can determine what images they see on the front page.
+    Users have the option of filtering images based on either 'Most Recent' or 'Most Popular'.</p>
+
+
+<h3>Creating an Account</h3>
+
+<p> - To access most functions on this site users must first create an account.</p>
+
+<p> - Users do not need to create account if they only wish to view user submitted content.</p>
+
+<p> - Users do not need to pay for access or the right to submit content on this site.</p>
+
+<p> - The setup process is simple and only requires .</p>
